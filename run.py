5934087492cfb6c2d4b6ccd06da8b8a86b54e3b7
Regan Boreland <reganboreland@hotmail.co.uk>
2024-07-17 15:58:51 +0000

    add function to randomly place ship locations

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ GUESS_BOARD = [[' '] * 6 for i in range(6)]
 ROWS = [0, 1, 2, 3, 4, 5]
 COLUMNS = [0, 1, 2, 3, 4, 5]
 
-
+#displays the board to the player
 def display_board(board):
     print("  0 1 2 3 4 5")
     print(" -------------")
@@ -20,7 +20,8 @@ def display_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
-def users_turns(number):
+#requests the users guess for a row and a column
+def users_turns(input):
     row_guesses = int(input("Please pick a row (0-5): "))
     while row_guesses not in ROWS:
         print("This row doesn't exist")
@@ -29,22 +30,30 @@ def users_turns(number):
     while column_guesses not in COLUMNS:
         print("This column doens't exist")
         column_guesses = int(input("Please pick a column (0-5): "))
-    validate_users_turns()
+    return ROWS[row_guesses], COLUMNS[column_guesses]
 
-def validate_users_turns():
-    pass
+#creates the ships locations on the hidden board
+#if the location already has a ship then the computer places a ship elsewhere
+def create_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,5), randint(0,5)
+        if board[ship_row] [ship_column] == '@':
+            ship_row, ship_column = randint(0,5), randint(0,5)
+        else:
+            board[ship_row][ship_column] = '@'
 
-def create_ships():
-    pass
-
+#increases the points counter if player hits the ship
 def count_players_hits():
     pass
 
+#starts the game 
 def start_game():
     print("Welcome to Battleships, the ultimate guessing game")
+    print("Todays board is a little different to normal, we're using zero index so the top left corner will be 0,0\nand the bottom right will be 5,5")
     print("This is your board for todays game")
     display_board(GUESS_BOARD)
     print("Lets sink their ships")
-    users_turns(number)
+    create_ships(HIDDEN_BOARD)
+    users_turns(input)
 
-start_game()
+
