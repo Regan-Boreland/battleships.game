b498a820920ebda3275013e56a618d2178877cf2
Regan Boreland <reganboreland@hotmail.co.uk>
2024-07-17 14:28:45 +0000

    add guess validation to users_turns() function

--- run.py
+++ run.py
@@ -8,27 +8,30 @@ from random import randint
 HIDDEN_BOARD = [[' '] * 6 for x in range(6)]
 GUESS_BOARD = [[' '] * 6 for i in range(6)]
 
-ROWS = [0, 1, 2, 3, 4]
-COLUMNS = ['A', 'B', 'C', 'D', 'E', 'F' ]
+ROWS = [0, 1, 2, 3, 4, 5]
+COLUMNS = [0, 1, 2, 3, 4, 5]
 
 def start_game():
     pass
 
 def display_board(board):
-    print("  A B C D E F")
+    print("  0 1 2 3 4 5")
     print(" -------------")
     row_number = 0
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
-display_board(GUESS_BOARD)
-
-def users_turn():
-    pass
-
-def validate_users_turn():
-    pass
+def users_turns():
+    row_guesses = int(input("Please pick a row (0-5): "))
+    if row_guesses not in ROWS:
+        print("This row doesn't exist")
+        row_guesses = int(input("Please pick a row (0-5): "))
+    column_guesses = int(input("Please pick a column (0-5): "))
+    if column_guesses not in COLUMNS:
+        print("This column doens't exist")
+        column_guesses = int(input("Please pick a column (0-5): "))
+    return row_guesses, column_guesses
 
 def create_ships():
     pass
