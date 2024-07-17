31da44f444fba666fd8f4f770b0710bcaa4fb5ed
Regan Boreland <reganboreland@hotmail.co.uk>
2024-07-17 12:34:42 +0000

    add base function names

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Legend
+
+def start_game():
+    pass
+
+def display_board():
+    pass
+
+def users_turn():
+    pass
+
+def validate_users_turn():
+    pass
+
+def create_ships():
+    pass
