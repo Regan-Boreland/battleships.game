c9c00d44666c33978b87ab37e76f73fe539e332d
Regan Boreland <reganboreland@hotmail.co.uk>
2024-07-17 14:38:45 +0000

    add welcome message to start_game()

--- run.py
+++ run.py
@@ -11,8 +11,6 @@ GUESS_BOARD = [[' '] * 6 for i in range(6)]
 ROWS = [0, 1, 2, 3, 4, 5]
 COLUMNS = [0, 1, 2, 3, 4, 5]
 
-def start_game():
-    pass
 
 def display_board(board):
     print("  0 1 2 3 4 5")
@@ -22,19 +20,31 @@ def display_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
-def users_turns():
+def users_turns(number):
     row_guesses = int(input("Please pick a row (0-5): "))
-    if row_guesses not in ROWS:
+    while row_guesses not in ROWS:
         print("This row doesn't exist")
         row_guesses = int(input("Please pick a row (0-5): "))
     column_guesses = int(input("Please pick a column (0-5): "))
-    if column_guesses not in COLUMNS:
+    while column_guesses not in COLUMNS:
         print("This column doens't exist")
         column_guesses = int(input("Please pick a column (0-5): "))
-    return row_guesses, column_guesses
+    validate_users_turns()
+
+def validate_users_turns():
+    pass
 
 def create_ships():
     pass
 
 def count_players_hits():
     pass
+
+def start_game():
+    print("Welcome to Battleships, the ultimate guessing game")
+    print("This is your board for todays game")
+    display_board(GUESS_BOARD)
+    print("Lets sink their ships")
+    users_turns(number)
+
+start_game()
