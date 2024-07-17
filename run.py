f37dd8b11a4402597acb9a5cf96e1badb2f6837c
Regan Boreland <reganboreland@hotmail.co.uk>
2024-07-17 13:27:45 +0000

    add function to display board in terminal

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from random import randint
 # ' ' represents available location
 
 HIDDEN_BOARD = [[''] * 5 for x in range(5)]
-GUESS_BOARD = [[''] * 5 for x in range(5)]
+GUESS_BOARD = [[''] * 5 for i in range(5)]
 
 ROWS = [0, 1, 2, 3, 4]
 COLUMNS = [0, 1, 2, 3, 4]
@@ -14,11 +14,15 @@ COLUMNS = [0, 1, 2, 3, 4]
 def start_game():
     pass
 
-def display_board():
-    print("  0 1 2 3 4")
-    print("  ---------")
+def display_board(board):
+    print(" 01234")
+    print(" -----")
+    row_number = 0
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
 
-display_board()
+display_board(GUESS_BOARD)
 
 def users_turn():
     pass
