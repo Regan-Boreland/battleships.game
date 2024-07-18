5d930d4fb33f1100ecc52bab455db3d8c4010cf6
Regan Boreland <reganboreland@hotmail.co.uk>
2024-07-18 14:28:48 +0000

    change input validation to try/except statements rather than while loops

--- run.py
+++ run.py
@@ -11,6 +11,9 @@ GUESS_BOARD = [[' '] * 6 for i in range(6)]
 ROWS = [0, 1, 2, 3, 4, 5]
 COLUMNS = [0, 1, 2, 3, 4, 5]
 
+USER_LIVES = 10
+MAXIMUM_POINTS = 5
+
 def display_board(board):
     """
     displays the board to the player
@@ -22,19 +25,21 @@ def display_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
-def users_turns(input):
-    """
-    requests players guess on a row and column
-    """
-    row_guesses = int(input("Please pick a row (0-5): "))
-    while row_guesses not in ROWS:
-        print("This row doesn't exist")
-        row_guesses = int(input("Please pick a row (0-5): "))
-    column_guesses = int(input("Please pick a column (0-5): "))
-    while column_guesses not in COLUMNS:
-        print("This column doens't exist")
-        column_guesses = int(input("Please pick a column (0-5): "))
-    return ROWS[row_guesses], COLUMNS[column_guesses]
+def users_turn(input):
+    while True:
+        try:
+            row_guess = int(input("Pick a row (between 0-5): "))
+            if row_guess in ROWS:
+                break
+        except KeyError:
+            print("Please enter a row on the board")
+    while True:
+        try:
+            column_guess = int(input("Pick a column (between 0-5): "))
+            if column_guess in COLUMNS:
+                break
+        except KeyError:
+            print("Please enter a column on the board")
 
 def create_ships(board):
     """
@@ -42,7 +47,8 @@ def create_ships(board):
     if the location already has a ship then the computer places a ship elsewhere
     """
     for ship in range(5):
-        ship_row, ship_column = randint(0,5), randint(0,5)
+        ship_row = randint(0,5)
+        ship_column = randint(0,5)
         if board[ship_row] [ship_column] == '@':
             ship_row, ship_column = randint(0,5), randint(0,5)
         else:
@@ -50,19 +56,23 @@ def create_ships(board):
 
 #increases the points counter if player hits the ship
 def count_players_hits():
-    pass
-
+    """
+    increases players points if a ship is hit and decreases the amount of lives per turn
+    """
+    
 
 def start_game():
     """
     starts the game
     """
     print("Welcome to Battleships, the ultimate guessing game")
-    print("Todays board is a little different to normal, we're using zero index so the top left corner will be 0,0\nand the bottom right will be 5,5")
+    print("--------------------------------------------------")
+    print("Our game works a little different to a normal game,\nrather than playing against someone you will be given 10 lives,\nyou have to hit the 5 boats within the 10 lives\nor it's GAME OVER")
+    print("--------------------------------------------------")
     print("This is your board for todays game")
     display_board(GUESS_BOARD)
     print("Lets sink their ships")
     create_ships(HIDDEN_BOARD)
-    users_turns(input)
+    users_turn(input)
 
-
+start_game()
