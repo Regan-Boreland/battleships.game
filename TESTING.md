4c7cb2ac4eace90cfbfd9abef4bb9945ca8a36c4
Regan Boreland <reganboreland@hotmail.co.uk>
2024-07-20 07:47:28 +0000

    alter whitespace around operators and add space in comments for CI Python Linter recommendations

--- TESTING.md
+++ TESTING.md
@@ -24,4 +24,4 @@ The Battleships game was tested on the following browsers
 | Points remain at zero even when a target has been hit | Rather than using a separate function to increment the points, I decided to include an if statement in the start_game function incrementing the score if a taget was hit |
 | When the project was deployed, the player has no area to enter their row and column guess | This was solved by adding space after the user input|
 | When leaving the row/column guess empty and validating the empty input, the user input was displayed again without providing the user with a message | I used a try/except statement and provided the user with a message if the input was entered as empty |
-| User can enter the same hit ship coordinates and keep increasing their points |
+| User can enter the same hit ship coordinates and keep increasing their points | Add an additional if statement informing user that coordinates can't be reused |
--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from random import randint
 
 # Legend
 # @ represents ship locations
-# x represents missed 
+# x represents missed
 # ' ' represents available location
 # # represents hit ship
 
@@ -15,6 +15,7 @@ COLUMNS = [0, 1, 2, 3, 4, 5]
 row = ""
 column = ""
 
+
 def display_board(board):
     """
     displays the board to the player
@@ -26,17 +27,18 @@ def display_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
+
 def users_turn(input):
-    #validation for row guess
+    # validation for row guess
     global row_guess
     while True:
         try:
             row_guess = int(input("Pick a row (between 0-5): \n"))
             if row_guess in ROWS:
-                break 
+                break
         except ValueError:
             print("Please enter a row on the board")
-    #validation for column guess
+    # validation for column guess
     global column_guess
     while True:
         try:
@@ -44,22 +46,25 @@ def users_turn(input):
             if column_guess in COLUMNS:
                 break
         except ValueError:
-            print("Please enter a column on the board") 
+            print("Please enter a column on the board")
+
 
 def create_ships(board):
     """
     creates the ships locations on the hidden board
-    if the location already has a ship then the computer places a ship elsewhere
+    if the location already has a ship then the computer
+    places a ship elsewhere
     """
     for ship in range(5):
-        target_row = randint(0,5)
-        target_column = randint(0,5)
-        #uses randint to find a location on the board for a ship,
-        #if a location is already taken then run again till a location is found
+        target_row = randint(0, 5)
+        target_column = randint(0, 5)
+    # uses randint to find a location on the board for a ship,
+    # if a location is already taken then run again till a location is found
         if board[target_row][target_column] == '@':
-            target_row, target_column = randint(0,5), randint(0,5)
+            target_row, target_column = randint(0, 5), randint(0, 5)
         else:
             board[target_row][target_column] = '@'
+
 
 def start_game():
     """
@@ -78,7 +83,7 @@ def start_game():
     points = 0
     while turns > 0:
         users_turn(input)
-        #adds symbols to guess and hidden board based on user input
+    # adds symbols to guess and hidden board based on user input
         if GUESS_BOARD[row_guess][column_guess] == 'X' or GUESS_BOARD[row_guess][column_guess] == '#':
             print("You've already used those coordinates, pick again")
         elif HIDDEN_BOARD[row_guess][column_guess] == '@':
@@ -89,16 +94,16 @@ def start_game():
         else:
             print("You missed! Focus and sink their ships")
             GUESS_BOARD[row_guess][column_guess] = 'X'
-            turns -=1
+            turns -= 1
             print(f"you have {turns} lives remaining")
-        #increases points if a location on hidden board and guess board align
+        # increases points if a location on hidden board and guess board align
         if GUESS_BOARD[row_guess][column_guess] == '#':
-            points += 1 
+            points += 1
             print(f"You have sunk {points} ships")
         if points == 4:
             print("There's only one target left")
             print("Hurry up and end this before we're out of ammunition")
-        #ends the game when points reach 5 or turns reach 0
+        # ends the game when points reach 5 or turns reach 0
         if points == 5:
             print("Congratulations you sunk all their ships")
             print("GAME OVER!!!")
@@ -106,5 +111,6 @@ def start_game():
         if turns == 0:
             print("Wer're out of ammunition\nWe'll get them next time")
         display_board(GUESS_BOARD)
+
 
 start_game()
